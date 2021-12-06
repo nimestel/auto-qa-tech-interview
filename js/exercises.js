5d3818325217c686e4c0bab315d8e34eee633d85
Tatyana Kozlova <tatyana.y.kozlova@raiffeisen.ru>
2021-12-06 13:55:44 +0300

    варианты сортировки

--- js/exercises.js
+++ js/exercises.js
@@ -37,7 +37,43 @@ function invert2() {
     console.log(inverted.join(''));
 }
 
+function bubbleSort() {
+    let arr = [5, 10, 1, 0, 3, 4];
+    console.log('arr before:', arr);
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        console.log('* i:', i);
+        // после первой (i=0) серии проходок наибольшее число всплывет в конец
+        // поэтому в цикле j мы уменьшаем число проходок на кол-во вставших на свое место чисел
+        for (let j = 0; j < arr.length - i; j++) {
+            console.log('j:', j);
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+            console.log(arr);
+        }
+    }
+    console.log('arr after:', arr);
+}
+
+function nativeSort() {
+    let arr = [5, 10, 1, 0, 3, 4];
+    console.log('arr before:', arr);
+
+    // asc sort
+    arr.sort((a, b) => a - b);
+
+    console.log('arr asc sort:', arr);
+
+    // desc sort
+    arr.sort((a, b) => b - a);
+
+    console.log('arr desc sort:', arr);
+}
+
 //fizzbuzz1();
 //fizzbuzz2();
 //invert1();
 //invert2();
+//bubbleSort();
+//nativeSort();
