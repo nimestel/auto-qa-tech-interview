7756c879ad6cca9ca60ae47cb4501839d9be0fcf
Tatyana Kozlova <tatyana.y.kozlova@raiffeisen.ru>
2022-01-09 17:46:45 +0300

    impl calculator with tests

--- js/exercises.js
+++ js/exercises.js
@@ -77,3 +77,145 @@ function nativeSort() {
 //invert2();
 //bubbleSort();
 //nativeSort();
+
+
+/** impl calculator and tests */
+
+const ErrorMessages = Object.freeze({
+    notNumber: 'Please enter args in integer format',
+    isNull: 'Please enter second argument not a 0!',
+});
+
+class Calculator {
+    add(num1, num2) {
+        function addition(args) {
+            let res = 0;
+            [...args].forEach((arg) => (res += arg));
+            return res;
+        }
+
+        let checkArgInvalid = this.checkArgsInvalid(...arguments);
+        return checkArgInvalid ? checkArgInvalid : addition(arguments);
+    }
+    divide(num1, num2) {
+        let checkArgInvalid = this.checkArgsInvalid(...arguments);
+        return checkArgInvalid ? checkArgInvalid : num2 === 0 ? ErrorMessages.isNull : num1 / num2;
+    }
+    multi(num1, num2) {
+        function multiplication(args) {
+            let res = 1;
+            [...args].forEach((arg) => (res *= arg));
+            return res;
+        }
+
+        let checkArgInvalid = this.checkArgsInvalid(...arguments);
+        return checkArgInvalid ? checkArgInvalid : multiplication(arguments);
+    }
+    minus(num1, num2) {
+        let checkArgInvalid = this.checkArgsInvalid(...arguments);
+        return checkArgInvalid ? checkArgInvalid : num1 - num2;
+    }
+
+    checkArgsInvalid(...args) {
+        function isValid(a) {
+            return (Number.isInteger(a) && a) || a === 0;
+        }
+        if (args.every(isValid)) {
+            return false;
+        }
+        return ErrorMessages.notNumber;
+    }
+}
+
+const calc = new Calculator();
+
+const data = {
+    add: [
+        {
+            args: [1, 1, 1],
+            result: 3,
+        },
+        {
+            args: [0, 0],
+            result: 0,
+        },
+        {
+            args: [NaN, 4],
+            result: ErrorMessages.notNumber,
+        },
+        {
+            args: [undefined, 0],
+            result: ErrorMessages.notNumber,
+        },
+        {
+            args: [null, 5],
+            result: ErrorMessages.notNumber,
+        },
+        {
+            args: [99999999, 3],
+            result: 100000002,
+        },
+    ],
+    divide: [
+        {
+            args: [1, 1],
+            result: 1,
+        },
+        {
+            args: [1, 0],
+            result: ErrorMessages.isNull,
+        },
+        {
+            args: [0, 1],
+            result: 0,
+        },
+        {
+            args: [12, 4],
+            result: 3,
+        },
+        {
+            args: [4, 100],
+            result: 0.04,
+        },
+    ],
+    multi: [
+        {
+            args: [0, 0],
+            result: 0,
+        },
+        {
+            args: [1, 0],
+            result: 0,
+        },
+        {
+            args: [2, 3],
+            result: 6,
+        },
+    ],
+};
+
+const ConsoleColors = {
+    white: '\x1b[0m',
+    red: '\x1b[31m',
+    green: '\x1b[32m',
+};
+
+function test(set, operation) {
+    console.log(ConsoleColors.white, '==================');
+    console.log(ConsoleColors.white, 'operation:', operation);
+    console.log(ConsoleColors.white, 'args: ', set.args);
+    console.log(ConsoleColors.white, 'expected result: ', set.result);
+
+    const res = calc[operation](...set.args);
+    console.log(ConsoleColors.white, 'actual result: ', res);
+
+    res === set.result
+        ? console.log(ConsoleColors.green, 'right answer! :)')
+        : console.log(ConsoleColors.red, 'wrong answer! :(');
+}
+
+Object.keys(data).forEach((operation) => {
+    data[operation].forEach((set) => {
+        test(set, operation);
+    });
+});
