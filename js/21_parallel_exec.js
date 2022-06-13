fa894148d11b5599cd1e49eccd6688259d2617c8
Tatyana Kozlova <37553224+nimestel@users.noreply.github.com>
2022-06-13 22:37:02 +0300

    upd parallel

--- js/21_parallel_exec.js
+++ js/21_parallel_exec.js
@@ -1,1 +1,5 @@
+// см. event loop
+
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/EventLoop
+
+// https://developer.mozilla.org/ru/docs/conflicting/Learn/JavaScript/Asynchronous/Introducing
